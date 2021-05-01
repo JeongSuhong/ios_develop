ae0c335455da4703a98605364a1a5a88130d961d
JeongSuhong <ghjkdfe@gmail.com>
2021-05-01 17:39:23 +0900

    Upload Chapter 1

--- ios_develop/ViewController.swift
+++ ios_develop/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloLabel: UILabel!
+    @IBOutlet var nameField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sendName(_ sender: UIButton) {
+        helloLabel.text = "Hello, " + nameField.text!
+    }
 }
 
